1a273077409b3aa57ea7d8e7b02e831227616729
Demitroi Marshall <checo1085@hotmail.com>
2025-03-26 14:02:22 -0700

    Add html and css linters

--- eslint.config.js
+++ eslint.config.js
@@ -5,35 +5,36 @@ import reactRefresh from 'eslint-plugin-react-refresh';
 import stylisticJsx from '@stylistic/eslint-plugin-jsx';
 
 export default [
-  { ignores: ['dist'] },
-  {
-    files: ['**/*.{js,jsx}'],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-      parserOptions: {
-        ecmaVersion: 'latest',
-        ecmaFeatures: { jsx: true },
-        sourceType: 'module',
-      },
+    { ignores: ['dist'] },
+    {
+        files: ['**/*.{js,jsx}'],
+        languageOptions: {
+            ecmaVersion: 2020,
+            globals: globals.browser,
+            parserOptions: {
+                ecmaVersion: 'latest',
+                ecmaFeatures: { jsx: true },
+                sourceType: 'module',
+            },
+        },
+        plugins: {
+            'react-hooks': reactHooks,
+            'react-refresh': reactRefresh,
+            '@stylistic/jsx': stylisticJsx,
+        },
+        rules: {
+            ...js.configs.recommended.rules,
+            ...reactHooks.configs.recommended.rules,
+            'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
+            'react-refresh/only-export-components': [
+                'warn',
+                { allowConstantExport: true },
+            ],
+            semi: "warn",
+            indent: ['warn', 4],
+            "prefer-const": "warn",
+            '@stylistic/jsx/jsx-indent': ['warn', 4],
+            //indent: ['warn', 4],
+        },
     },
-    plugins: {
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
-      '@stylistic/jsx': stylisticJsx,
-    },
-    rules: {
-      ...js.configs.recommended.rules,
-      ...reactHooks.configs.recommended.rules,
-      'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
-      semi: "warn",
-      "prefer-const": "warn",
-      '@stylistic/jsx/jsx-indent': ['warn', 4],
-      //indent: ['warn', 4],
-    },
-  },
 ];
--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,14 @@ import AddItem from './AddItem.jsx';
 import ItemStats from './ItemStats.jsx';
 
 const initialSales = [
-    {id: 0, name: 'Mint', sales: 3},
-    {id: 1, name: 'Mushroom', sales: 8},
-    {id: 2, name: 'Potato', sales: 10},
-    {id: 3, name: 'Capsicum', sales: 12},
-    {id: 4, name: 'Cauliflower', sales: 7},
-    {id: 5, name: 'Garlic', sales: 11},
-    {id: 6, name: 'Ginger', sales: 1},
-    {id: 7, name: 'Eggplant', sales: 15}
+    { id: 0, name: 'Mint', sales: 3 },
+    { id: 1, name: 'Mushroom', sales: 8 },
+    { id: 2, name: 'Potato', sales: 10 },
+    { id: 3, name: 'Capsicum', sales: 12 },
+    { id: 4, name: 'Cauliflower', sales: 7 },
+    { id: 5, name: 'Garlic', sales: 11 },
+    { id: 6, name: 'Ginger', sales: 1 },
+    { id: 7, name: 'Eggplant', sales: 15 }
 ];
 
 let nextID = initialSales.length;
@@ -32,7 +32,7 @@ const App = () => {
     const handleAddSale = (name) => {
         const newSales = [
             ...sales,
-            {id: nextID++, name: name, sales: 0}
+            { id: nextID++, name: name, sales: 0 }
         ];
         setSales(newSales);
     };
@@ -57,18 +57,18 @@ const App = () => {
         <main>
             <AddItem onAddItem={handleAddSale} />
 
-            <hr/>
+            <hr />
 
-            {sales.map(({id, name, sales}) => (
+            {sales.map(({ id, name, sales }) => (
                 <Item key={id}
                     id={id}
                     name={name}
                     sales={sales}
                     onChageSale={handleChangeSale}
-                    onDeleteSale={handleDeleteSale}/>
+                    onDeleteSale={handleDeleteSale} />
             ))}
 
-            <hr/>
+            <hr />
 
             <ItemStats totalItems={totalItems}
                 totalSales={totalSales}
